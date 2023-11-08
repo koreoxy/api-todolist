370ba3aef92932160a6a197cf70711bcdf7c5d55
koreoxy <rayssankn@gmail.com>
2023-11-08 18:15:43 +0700

    add login route

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,7 @@
 import User from '../models/UserModel.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -14,5 +15,26 @@ export const register = async (req, res, next) => {
     res.status(201).json('User berhasil di daftarkan!!');
   } catch (error) {
     next(errorHandler(550, 'name and email already exist'));
+  }
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User tidak ditemukan!'));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Password Salah!'));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_KEY);
+
+    // REMOVE PASSWORD when TO SEND CLIENT
+    const { password: pass, ...rest } = validUser._doc;
+
+    res.cookie('token_access', token, { httOnly: true }).status(200).json(rest);
+  } catch (error) {
+    next(error);
   }
 };
